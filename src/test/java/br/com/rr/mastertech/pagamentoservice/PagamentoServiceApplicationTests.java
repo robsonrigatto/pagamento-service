0b885f0e265ec6679dad086e30ddcdeb3165db2d
robsonrigatto <rigatto.robson@gmail.com>
2020-03-11 14:27:10 -0300

    first commit

---
+++ src/test/java/br/com/rr/mastertech/pagamentoservice/PagamentoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rr.mastertech.pagamentoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
