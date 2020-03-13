94bb2de8198747da7e0c7715fc8982f04fdbea98
robsonrigatto <rigatto.robson@gmail.com>
2020-03-13 09:47:36 -0300

    hystrix dependency + ribbon configuration

--- src/main/java/br/com/rr/mastertech/pagamento/PagamentoServiceApplication.java
+++ src/main/java/br/com/rr/mastertech/pagamento/PagamentoServiceApplication.java
@@ -1,11 +1,16 @@
 package br.com.rr.mastertech.pagamento;
 
+import br.com.rr.mastertech.pagamento.configuration.RibbonConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@RibbonClients(defaultConfiguration = RibbonConfiguration.class)
+@EnableCircuitBreaker
 public class PagamentoServiceApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/br/com/rr/mastertech/pagamento/configuration/RibbonConfiguration.java
@@ -0,0 +1,15 @@
+package br.com.rr.mastertech.pagamento.configuration;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.WeightedResponseTimeRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RibbonConfiguration {
+
+    @Bean
+    public IRule ribbonRule() {
+        return new WeightedResponseTimeRule();
+    }
+}
